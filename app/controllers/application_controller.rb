ac16e14b0b24689b28a1b040b914f38c6e58feb3
ydecatheu <ydecatheu-presta@ventadis.fr>
2019-02-04 21:53:52 +0100

    devise view updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   include Pundit
   
@@ -10,6 +11,14 @@ class ApplicationController < ActionController::Base
     else 
       request.env['omniauth.origin'] || stored_location_for(resource) || candidates_path
     end  
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 
   # Pundit: white-list approach.
