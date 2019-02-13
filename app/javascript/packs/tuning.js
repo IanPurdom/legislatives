3264dc4692be8cdf0aacd122193008a73b029be3
Yan DC <ianpurdom75@gmail.com>
2019-02-14 00:05:58 +0100

    front job index and show

--- app/javascript/packs/tuning.js
+++ app/javascript/packs/tuning.js
@@ -1,23 +1,48 @@
 
 function changeColor() {
   $('.status').each(function(i, obj) {
+	  if (obj.innerHTML == "OPEN") 
+		{obj.classList.add("green");
+		obj.innerHTML = "CAN";}
+	  else if (obj.innerHTML == "PENDING_SD")
+	  	{obj.classList.add("orange");
+		obj.innerHTML = "SD";}
+	  else if (obj.innerHTML == "PENDING_DNF")
+	  	{obj.classList.add("yellow");
+		obj.innerHTML = "DNF";}
+	  else if (obj.innerHTML == "PENDING_COM")
+	  	{obj.classList.add("red");
+		obj.innerHTML = "COM";}
+	  else if (obj.innerHTML == "PENDING_CANDIDATE")
+	  	{obj.classList.add("blue");
+		obj.innerHTML = "CAN";}
+	  else if (obj.innerHTML == "CLOSE")
+	  	{obj.classList.add("");}	
+	});
+}
+
+function changeColorShow() {
+  $('.candidate-status').each(function(i, obj) {
 	  if (obj.innerHTML == "En cours de création") 
 		{obj.classList.add("green");}
-	  else if (obj.innerHTML == "En attente de validation SD")
-	  	{obj.classList.add("blue");}
-	  else if (obj.innerHTML == "En attente de validation DNF")
+	  else if (obj.innerText == "En attente de validation SD")
+  		{console.log("cococu")
+	  	obj.classList.add("orange");}
+	  else if (obj.innerText == "En attente de validation DNF")
 	  	{obj.classList.add("yellow");}
-	  else if (obj.innerHTML == "En attente de validation Pôle Com.")
-	  	{obj.classList.add("orange");}
-	  else if (obj.innerHTML == "En attende de validation finale du candidat")
+	  else if (obj.innerText == "En attente de validation Pôle Com.")
 	  	{obj.classList.add("red");}
-	  else if (obj.innerHTML == "Candidature close")
-	  	{obj.classList.add("bg-success");}	
+	  else if (obj.innerText == "En attende de validation finale du candidat")
+	  	{obj.classList.add("blue");}
+	  else if (obj.innerText == "CLOSE")
+	  	{obj.classList.add("");}	
 	});
 }
 
 var $ = require('jquery');
 window.jQuery = $;
 window.$ = $;
-window.changeColor = changeColor
 window.onload = changeColor()
+window.onload = changeColorShow()
+window.changeColor = changeColor
+window.changeColorShow = changeColorShow
